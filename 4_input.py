cc0a4f04983efde2fb407bbdc2dd9f00fdb8ddba
theeasylearn <theeasylearn@gmail.com>
2026-05-25 19:46:23 +0530

    input in python

---
+++ 4_input.py
@@ -0,0 +1,17 @@
+# write a program to accept input from user using input function
+# name, age, weight
+
+#variable-name = input("input message ")
+name = input("what is your name?")
+print("your name is ",name)
+
+age = input("what is your age") 
+weight = input("What is your weight in kg?")
+
+#convert it into integer
+age = int(age)
+
+#convert it into float 
+weight = float(weight)
+
+print("age = ",age," weight = ",weight)
