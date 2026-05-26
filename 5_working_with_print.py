f7601ee9a0384e1d554dc3c20d2d502801d7c56f
theeasylearn <theeasylearn@gmail.com>
2026-05-26 19:44:43 +0530

    revision

---
+++ 5_working_with_print.py
@@ -0,0 +1,7 @@
+print('hello')
+print("we have started learning python")
+print("""THE EASYLEARN ACADEMY
+105, eva surbhi, opp aksharwadi temple, 
+bhavnagar - 364001
+Gujarat - india """)
+print('one'); print("two"); print('three')
---
+++ 6_working_with_variable.py
@@ -0,0 +1,19 @@
+#create variable
+# variable-name = value
+name = "the easylearn academy"
+year = 2026
+weight = 80.25
+gender = True 
+
+#display variable
+print(name)
+print("year = ",year)
+print("weight = ",weight)
+print("gender =",gender)
+
+name = 100
+year = "two thousand and twenty six"
+weight = False 
+gender = "Male"
+
+print(name,year,weight,gender)
