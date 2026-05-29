bf6f19267b4296b41289fcc8742bea7756d97323
theeasylearn <theeasylearn@gmail.com>
2026-05-29 19:36:31 +0530

    list_1

---
+++ 10_list_1.py
@@ -0,0 +1,20 @@
+#example of list 
+fruits = ['mango','apple','banana','orange','kiwi']
+
+print(fruits) 
+print(fruits[0]) #mango
+print(fruits[2]) # banana
+print(fruits[4]) # kiwi
+# print(fruits[5]) #error
+print(fruits[0:2]) #mango apple
+print(fruits[3:]) # orange kiwi
+
+list = [100,True,'car','ac',False]
+print('good bye')
+print(list)
+print(fruits + list)
+
+#print fruits 5 times
+print(fruits * 5,list)
+
+
